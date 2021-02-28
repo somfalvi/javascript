91d91611411021cd13ada07b403b4ce34ba79308
captainbaloo <somfalvib@protonmail.com>
2021-02-27 17:53:24 -0700

    add statistical formula

---
+++ phi.js
@@ -0,0 +1,6 @@
+//correlation is a measure of dependece between statistical variables
+//range:-1 to 1;
+//0 means the variables are not correlated;
+//1 means tge two variables are perfectly related
+//-1 means the variables are perfectly related but they are opposites
+
