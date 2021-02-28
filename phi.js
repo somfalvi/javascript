7ebee558a0883bbb964683907ef0976d6b97eed0
captainbaloo <somfalvib@protonmail.com>
2021-02-27 18:18:44 -0700

    fix typos

--- phi.js
+++ phi.js
@@ -4,3 +4,12 @@
 //1 means tge two variables are perfectly related
 //-1 means the variables are perfectly related but they are opposites
 
+function phi(table) {
+    return(table[3] * table[0] - table[2] * table[1])/
+	Math.sqrt((table[2] + table[3]) *
+		  (table[0] + table[1]) *
+		  (table[1] + table[3]) *
+		  (table[0] + table[2]));
+}
+
+console.log(phi([76,9,4,1]));
